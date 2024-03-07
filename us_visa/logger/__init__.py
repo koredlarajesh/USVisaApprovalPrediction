36900b6e9cf5a65c3b9397c76a526bcba5889aeb
USVisa <rajesh.koredla25@gmail.com>
2024-03-07 11:46:32 +0530

    logging added

---
+++ us_visa/logger/__init__.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from from_root import from_root
+from datetime import datetime
+
+log_file = f"{datetime.now().strftime('%m_%d_%y_%h_%m_%s')}.log"
+log_dir = "logs"
+logs_path=os.path.join(from_root(),log_dir,log_file)
+os.makedirs(log_dir,exist_ok=True)
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+)
